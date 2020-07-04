00ed6fed668aaec1d1d3bf1a69b3a4749d290768
Sayaka Ono <sayaka.ono.81@gmail.com>
2020-07-03 23:00:20 -0700

    write tests for Input success=true

--- src/input.test.js
+++ src/input.test.js
@@ -35,9 +35,22 @@ describe('render', () => {
     });
   });
   describe('word has been guessed', () => {
-    test('renders component without error', () => {});
-    test('does not renders input box', () => {});
-    test('does not renders submit button', () => {});
+    let wrapper;
+    beforeEach(() => {
+      wrapper = setup({ success: true });
+    });
+    test('does not render component without error', () => {
+      const component = findByTestAttr(wrapper, 'component-input');
+      expect(component.length).toBe(1);
+    });
+    test('does not render input box', () => {
+      const inputBox = findByTestAttr(wrapper, 'input-box');
+      expect(inputBox.length).toBe(0);
+    });
+    test('does not render submit button', () => {
+      const submitButton = findByTestAttr(wrapper, 'submit-button');
+      expect(submitButton.length).toBe(0);
+    });
   });
 });
 
