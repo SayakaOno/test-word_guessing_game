aa7c650834ab8e2898474614a70c0b8ad3ad8b98
Sayaka Ono <sayaka.ono.81@gmail.com>
2020-07-04 20:04:47 -0700

    set up guessWord tests

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
-import { cobineReducers, combineReducers } from 'redux';
+import { combineReducers } from 'redux';
 import success from './successReducer';
+import guessedWords from './guessedWordsReducer';
 
 export default combineReducers({
-  success
+  success,
+  guessedWords
 });
