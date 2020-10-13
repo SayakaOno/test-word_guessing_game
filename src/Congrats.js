50ef3c93345a1d6af0d3352accdafa054d31693f
Sayaka Ono <sayaka.ono.81@gmail.com>
2020-10-12 21:13:01 -0700

    add New word button with tests

--- src/Congrats.js
+++ src/Congrats.js
@@ -1,21 +1,37 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { getSecretWord } from './actions';
 
-const Congrats = props => {
+export const UnconnectedCongrats = props => {
   if (props.success) {
     return (
-      <div data-test="component-congrats" className="alert alert-success">
-        <span data-test="congrats-message">
-          Congratulations! You guessed the word!
-        </span>
-      </div>
+      <>
+        <div data-test="component-congrats" className="alert alert-success">
+          <span data-test="congrats-message">
+            Congratulations! You guessed the word!
+          </span>
+        </div>
+        <button
+          data-test="new-word-button"
+          className="btn btn-primary mb-2"
+          type="submit"
+          onClick={props.getSecretWord}
+        >
+          New Word
+        </button>
+      </>
     );
   } else {
     return <div data-test="component-congrats" />;
   }
 };
 
-Congrats.propTypes = {
+UnconnectedCongrats.propTypes = {
   success: PropTypes.bool.isRequired
 };
-export default Congrats;
+
+export default connect(
+  null,
+  { getSecretWord }
+)(UnconnectedCongrats);
--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import { findByTestAttr, checkProps } from '../test/testUtils';
-import Congrats from './Congrats';
+import { findByTestAttr, checkProps, storeFactory } from '../test/testUtils';
+import Congrats, { UnconnectedCongrats } from './Congrats';
 
 const defaultProps = { success: false };
 
 const setup = (props = {}) => {
   const setupProps = { ...defaultProps, ...props };
-  return shallow(<Congrats {...setupProps} />);
+  const store = storeFactory(defaultProps);
+  const wrapper = shallow(
+    <UnconnectedCongrats store={store} {...setupProps} />
+  );
+  return wrapper;
 };
 
 test('renders without error', () => {
@@ -32,4 +36,30 @@ test("renders non-empty congrats message when 'success' prop is true", () => {
 test('does not throw warning with expected props', () => {
   const expectedProps = { success: false };
   checkProps(Congrats, expectedProps);
+});
+
+test("renders 'New word' button after successful guess", () => {
+  const wrapper = setup({ success: true });
+  const newWordButton = findByTestAttr(wrapper, 'new-word-button');
+  expect(newWordButton.length).toBe(1);
+});
+
+test("renders 'New word' button after successful guess", () => {
+  const wrapper = setup({ success: true });
+  const newWordButton = findByTestAttr(wrapper, 'new-word-button');
+  expect(newWordButton.length).toBe(1);
+});
+
+test("calls 'getSecretWord' when 'new word' button is clicked", () => {
+  const getSecretWordMock = jest.fn();
+  const props = {
+    getSecretWord: getSecretWordMock,
+    success: true
+  };
+  const wrapper = setup(props);
+  const newWordButton = findByTestAttr(wrapper, 'new-word-button');
+  newWordButton.simulate('click');
+
+  const getSecretWordCallCount = getSecretWordMock.mock.calls.length;
+  expect(getSecretWordCallCount).toBe(1);
 });
